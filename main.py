010e910ec83b9c0d66ec7055b06b8f9d9e8a997a
Efekangimbil <134960102+Efekangimbil@users.noreply.github.com>
2023-06-05 20:44:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+uzunluk=int(input("Kaç birim uzunluğunda bir şifre istersiniz?:"))
+
+for i in range(uzunluk):
+    sifre=random.choice(["+","-","/","*","!","&","#","?","=","@","a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"])
+    print(sifre,end="")
+
